8b335fdd64fdea7b5591c4c91d7083d4c0d0a6cd
Marie <76613254+zhurawlenokk@users.noreply.github.com>
2025-11-27 13:51:35 +0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,3 @@
-//прописать типы для всех сущностей
-
-/**
- * что-то в духе:
- * export interface IPlayer {
- *   id: number;
- *   name: string;
- *   image: string;
- *   hp: number;
- *   setHeal: (hp: number) => void;
- *   dices: TCombinationResult[];
- *   countRolls: number;
- *   damage: number;
- * */
-
 export interface ApiCharacters {
     id: number;
     name: string;
